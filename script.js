7ff65b8bdd56d9f085b6c7344a197fe479129fb1
saeed.su <saeed.su@markitasdone.com>
2021-08-05 17:56:35 +0430

    added form listener

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var modal = document.getElementById("form");
 var btn = document.getElementById("form__open-btn");
 var span = document.getElementsByClassName("close")[0];
+const form = document.querySelector("#form")
+
 btn.onclick = function() {
   modal.style.display = "block";
 }
@@ -12,3 +14,8 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+
+})
