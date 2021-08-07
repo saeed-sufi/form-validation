74ba9f4b499a3c1a5b0384c397ff8905cdc32a47
masood <masooodm@yahoo.com>
2021-08-07 21:48:32 +0430

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var modal = document.getElementById("form");
 var btn = document.getElementById("form__open-btn");
 var span = document.getElementsByClassName("close")[0];
 const form = document.querySelector("#form")
+const task = document.querySelector("#task")
 
 btn.onclick = function() {
   modal.style.display = "block";
@@ -17,5 +18,14 @@ window.onclick = function(event) {
 
 form.addEventListener("submit", (e) => {
   e.preventDefault()
-
+  limitText (task)
 })
+
+function limitText(task) {
+	if (task.value.length > 4) {
+		task.value = task.value.substring(0, 4);
+    console.log (task.value)
+    const formControl = task.parentElement 
+    formControl.classList.add ("error")
+  }
+}
