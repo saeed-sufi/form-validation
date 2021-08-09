bc1b3b6634d360767a4d796a7f77ee2c0b5aa1a4
saeed.su <saeed.su@markitasdone.com>
2021-08-09 09:37:43 +0430

    showerror and showsuccess functions are added

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var modal = document.getElementById("form");
 var btn = document.getElementById("form__open-btn");
 var span = document.getElementsByClassName("close")[0];
 const form = document.querySelector("#form")
+const email = document.querySelector("#email")
 
 btn.onclick = function() {
   modal.style.display = "block";
@@ -17,5 +18,30 @@ window.onclick = function(event) {
 
 form.addEventListener("submit", (e) => {
   e.preventDefault()
-
+  checkEmail(email)
 })
+
+function checkEmail(email) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+  isValidEmail = re.test(String(email.value).toLowerCase())
+
+  if (!isValidEmail && email.value.trim() != "") {
+    showError(email, "Email is not valid.")
+  } else if (isValidEmail && email.value.trim() != "") {
+    showSuccess(email)
+    isValidEmail = true
+  }
+}
+
+function showError(element, message) {
+  const formControl = element.parentElement
+  formControl.className = "form-control error"
+  formControl.querySelector("small").innerText = message
+}
+
+function showSuccess(element) {
+  const formControl = element.parentElement
+  formControl.className = "form-control success"
+}
